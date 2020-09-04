5b372cd4e07b112ab5540ea02e2fca42d62e4c8a
n4m-ward <65575926+n4m-ward@users.noreply.github.com>
2020-09-04 17:51:24 -0300

    adicionando tinnyMCE

--- articles/articlesControler.js
+++ articles/articlesControler.js
@@ -5,6 +5,9 @@ router.get('/articles',(req,res)=>{
     res.send("rotas de artigos")
 });
 
+router.get('/admin/articles/new',(req,res)=>{
+    res.render('../views/admin/articles/new');
+})
 
 
 module.exports = router;
