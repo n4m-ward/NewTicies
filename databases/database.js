3b5e59cdaae001da399cb5b9c1f618981ac30407
nam-ward <dragondroid321@gmail.com>
2021-01-24 11:53:03 -0300

    .

--- databases/database.js
+++ databases/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const connection = new Sequelize('heroku_b7e8ee8c9f3fe1b','b662165df25638','37406118',{
-    host:'us-cdbr-east-03.cleardb.com',
+    host:'us-cdbr-east-03.cleardb.com:3307',
     dialect:'mysql',
     timezone: "-03:00"
 });
