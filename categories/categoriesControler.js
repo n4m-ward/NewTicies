6e1b2a9870f1ca21e280f728738826cb615a2863
gabriel-ward <gabriel.lima@logcomex.com>
2022-05-01 11:45:09 -0300

    - refatorado rotas, removido dos respectivos controllers e colocado em um arquivo de rotas proprio. - alterado controllers pra estrutura de classes que são chamadas no arquivo de rotas

--- categories/categoriesControler.js
+++ categories/categoriesControler.js
@@ -1,110 +1,134 @@
-const express = require('express');
-const router =  express.Router();
 const Category = require('./Category');
 const slugify = require('slugify');
-const bodyParser = require('body-parser');
-const adminAuth = require('../middlewares/adminAuth');
+const Article = require("./articles/Article");
+
+class CategoriesControler {
+   renderNewCategoriesPage(req, res) {
+       var estaLogado = req.session.user;
+       Category.findAll().then(categories =>{
+           res.render('admin/categories/new',{estaLogado:estaLogado,categories:categories});
+       })
+   }
+
+   renderAllCategoriesPage(req, res) {
+       var estaLogado = req.session.user;
+
+       Category.findAll().then(categories =>{
+           res.render('admin/categories/index',{categories: categories,estaLogado:estaLogado})
+       });
+   }
+
+   renderEditCategoriesPage(req, res) {
+       var id = req.params.id;
+       var estaLogado = req.session.user;
+
+       if(isNaN(id)){
+           res.redirect('/admin/categories');
+       }
+       else{
+
+       }
+       Category.findByPk(id).then(category =>{
+           if(category != undefined){
+
+               res.render('admin/categories/edit',{
+                   category:category,
+                   estaLogado:estaLogado
+               });
 
 
-router.get('/admin/categories/new',adminAuth,(req,res)=>{
-    var estaLogado = req.session.user;
-    Category.findAll().then(categories =>{
-        res.render('admin/categories/new',{estaLogado:estaLogado,categories:categories});
-    })
+           } else{
+               res.redirect('/admin/categories');
+           }
+       }).catch(erro =>{
+           res.redirect('/admin/categories');
+       })
+   }
 
-});
+   saveCategories(req, res) {
+       var title = req.body.title;
+       if(title != undefined){
+           Category.create({
+               title:title,
+               slug:slugify(title)
+           }).then(()=>{
+               res.redirect("/admin/categories");
+           })
+       }
+       else{
+           res.redirect('/admin/categories/new');
+       }
+   }
 
-router.post("/categories/save",adminAuth,(req,res)=>{
-    var title = req.body.title;
-    if(title != undefined){
-        Category.create({
-            title:title,
-            slug:slugify(title)
-        }).then(()=>{
-            res.redirect("/admin/categories");
-        })
-    }
-    else{
-        res.redirect('/admin/categories/new');
-    }
-});
+   updateCategories(req, res) {
+       var id = req.body.id;
+       var title = req.body.title;
+       var slug = req.body.title;
 
-router.get('/admin/categories',adminAuth,(req,res)=>{
-    var estaLogado = req.session.user;
-    
-    Category.findAll().then(categories =>{
-        res.render('admin/categories/index',{categories: categories,estaLogado:estaLogado})
-    });
-});
+       Category.update({
 
-router.post('/categories/delete',adminAuth,(req,res)=>{
-    var id = req.body.id;
-    if(id != undefined){
-        if(!isNaN(id)){
+           title:title,
+           slug:slug
+       },{
+           where:{
+               id:id
+           }
+       }).then(()=>{
+           res.redirect('/admin/categories');
+       })
+   }
 
-            Category.destroy({
-                where:{
-                    id: id
-                }
-            }).then(() => {
-                res.redirect("/admin/categories");
-            })
+   deleteCategories(req, res) {
+       var id = req.body.id;
+       if(id != undefined){
+           if(!isNaN(id)){
 
-        }
-        else{
-            res.redirect("/admin/categories");
-        }
-    }
-    else{
-        res.redirect("/admin/categories");
-    }
+               Category.destroy({
+                   where:{
+                       id: id
+                   }
+               }).then(() => {
+                   res.redirect("/admin/categories");
+               })
 
-});
+           }
+           else{
+               res.redirect("/admin/categories");
+           }
+       }
+       else{
+           res.redirect("/admin/categories");
+       }
 
-router.get('/admin/categories/edit/:id',adminAuth,(req,res)=>{
-    var id = req.params.id;
-    var estaLogado = req.session.user;
+   }
 
-    if(isNaN(id)){
-        res.redirect('/admin/categories');
-    }
-    else{
+   renderPageCategoryBySlug(req, res) {
+       var estaLogado = req.session.user;
+       var slug = req.params.slug;
+       Category.findOne({
+           where:{
+               slug: slug
+           },
+           include: [{model: Article}]
+       }).then( category =>{
+           if(category != undefined){
 
-    }
-    Category.findByPk(id).then(category =>{
-        if(category != undefined){
+               Category.findAll().then(categories =>{
+                   res.render('index',{
+                       articles:category.articles,
+                       categories:categories,
+                       estaLogado:estaLogado
+                   })
+               })
 
-            res.render('admin/categories/edit',{
-                category:category,
-                estaLogado:estaLogado
-            });
-
-
-        } else{
-            res.redirect('/admin/categories');
-        }
-    }).catch(erro =>{
-        res.redirect('/admin/categories');
-    })
-
-})
-router.post('/categories/update',adminAuth,(req,res)=>{
-    var id = req.body.id;
-    var title = req.body.title;
-    var slug = req.body.title;
-
-    Category.update({
-
-            title:title,
-            slug:slug
-        },{
-        where:{
-            id:id
-        }
-    }).then(()=>{
-        res.redirect('/admin/categories');
-    })
-})
+           } else{
+               res.rendirect('/');
+           }
+       }).catch(err =>{
+           res.redirect('/')
+       })
+   }
+}
 
 
-module.exports = router;
+module.exports = new CategoriesControler();
